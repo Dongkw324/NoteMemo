a97946942bb236616db0846adc8d83371b7760d4
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-07-02 21:57:32 +0900

    NoteMemo_database

---
+++ app/src/main/java/com/kdw/notememo/model/Memo.kt
@@ -0,0 +1,12 @@
+package com.kdw.notememo.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "NoteEntity")
+data class Memo(
+
+    @PrimaryKey(autoGenerate = true)
+    var id : Int,
+
+)
