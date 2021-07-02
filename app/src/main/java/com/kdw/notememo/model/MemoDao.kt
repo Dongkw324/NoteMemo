9e5ed6534a5b9860748694ad5df2e5e1ba9accc3
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-07-02 23:27:22 +0900

    NoteMemo_database

--- app/src/main/java/com/kdw/notememo/model/MemoDao.kt
+++ app/src/main/java/com/kdw/notememo/model/MemoDao.kt
@@ -1,4 +1,16 @@
 package com.kdw.notememo.model
 
+import androidx.room.*
+
+@Dao
 interface MemoDao {
+    @get: Query("SELECT * FROM NoteEntity ORDER BY id DESC")
+    val displayMemo : List<Memo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMemo(memo : Memo)
+
+    @Delete
+    fun deleteMemo(memo : Memo)
+
 }
