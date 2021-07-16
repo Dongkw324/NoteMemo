30e28e4bda2e904bb40bc702b1ebfdd16ed471eb
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-07-16 22:27:27 +0900

    NoteMemo 새로 구현

--- app/src/main/java/com/kdw/notememo/MainActivity.kt
+++ app/src/main/java/com/kdw/notememo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.kdw.notememo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kdw.notememo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainFragment: MainFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this
+        mainFragment = MainFragment()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, mainFragment).commit()
     }
 }
---
+++ app/src/main/java/com/kdw/notememo/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.kdw.notememo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.kdw.notememo.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+}
