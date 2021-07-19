d948267383aa5f4a32c56017b7edf2f077139e52
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-07-19 15:36:29 +0900

    NoteMemo 새로 구현

--- app/src/main/java/com/kdw/notememo/MainActivity.kt
+++ app/src/main/java/com/kdw/notememo/MainActivity.kt
@@ -6,17 +6,11 @@ import androidx.databinding.DataBindingUtil
 import com.kdw.notememo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainFragment: MainFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding.lifecycleOwner = this
-        mainFragment = MainFragment()
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, mainFragment).commit()
     }
 }
