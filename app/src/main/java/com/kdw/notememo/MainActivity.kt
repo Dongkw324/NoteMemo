bfd075eb45480d312f90ff6035641bb5d1da4b4c
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-07-29 15:50:28 +0900

    notememo 새로 제작

--- app/src/main/java/com/kdw/notememo/MainActivity.kt
+++ app/src/main/java/com/kdw/notememo/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.kdw.notememo.adapter.MemoAdapter
 import com.kdw.notememo.databinding.ActivityMainBinding
 import com.kdw.notememo.viewModel.MemoViewModel
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        permissionCheck()
 
         memoAdapter = MemoAdapter(this)
         binding.memoRecyclerView.adapter = memoAdapter
@@ -42,4 +46,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun permissionCheck() {
+
+        val permission : PermissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                return
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                finish()
+            }
+
+        }
+
+        TedPermission.with(this)
+                .setPermissionListener(permission)
+                .setRationaleMessage("더 많은 기능을 사용하려면 권한이 필요합니다.")
+                .setDeniedMessage("[설정] -> [권한] 에서 권한을 설정할 수 있습니다.")
+                .setPermissions(android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check()
+    }
 }
